8d77a60dcdbdc19e51d76ed48ca610a3f046cb7d
George Alton <galton@evertz.com>
2016-10-28 02:20:33 +0100

    use pelican

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'George Alton'
+SITENAME = u'georgealton.com'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+         ('about', 'about.html')
+        )
+# Social widget
+SOCIAL = (
+          ('github', 'https://github.com/georgealton'),
+         )
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+THEME = '/Users/george/pelican-themes/Flex'
